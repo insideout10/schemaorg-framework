da5dd24a27ea6cf2481040edaa38d2d74a1bfc36
David Riccitelli <david@insideout.io>
2012-04-21 20:52:50 +0300

    fixed the GeoCoordinates by assigning the FriendlyName

--- schemas/GeoCoordinates.php
+++ schemas/GeoCoordinates.php
@@ -3,19 +3,28 @@
 class GeoCoordinates extends Thing implements ISchema {
 	
 	/**
-	 * @type Number or Text
+	 * Get the friendly name for this schema.
+	 * @return string The friendly name for this schema.
+	 */
+	public static function getFriendlyName() {
+		return 'GeoCoordinates';
+	}
+	
+	
+	/**
+	 * @type Text
 	 * @description The elevation of a location.
 	 */
 	public $elevation;
 	
 	/**
-	 * @type Number or Text
+	 * @type Text
 	 * @description The latitude of a location. For example 37.42242.
 	 */
 	public $latitude;
 	
 	/**
-	 * @type Number or Text
+	 * @type Text
 	 * @description The longitude of a location. For example -122.08585.
 	 */
 	public $longitude;
